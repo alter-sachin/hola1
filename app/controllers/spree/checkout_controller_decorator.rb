c4ed1e2a23dfc11ce9b9c413def0e23a7301d774
alter-sachin <guitarplayersachin@gmail.com>
2016-11-30 00:28:51 -0500

    added stuff again! pincode finally

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -67,27 +67,6 @@ module Spree
         # redirect_to edit_order_checkout_url(@order, :state => 'payment'), :notice => Spree.t(:complete_payu_checkout)
         render "spree/checkout/confirm_payu", layout: false
       end
-
-
-    
-  def time_delivery
-    # your code goes here
-    time =  Time.now.in_time_zone('Chennai').hour
-    date1 = DateTime.now.in_time_zone('Chennai').to_date + 2
-    date2 = DateTime.now.in_time_zone('Chennai').to_date + 3
-  	
-
-    if time>0 and time<13
-  	@delivery="Will be delivered to you between 2pm to 4pm on " + date1.to_s
-    	   #puts delivery
-    elsif time >=13 and time <17
-  	@delivery="Will be delivered to you between 6pm to 8pm on " + date1.to_s
-    	  #puts delivery
-    else
-  	@delivery="Will be delivered to you between 2pm to 4 pm on " + date2.to_s
-          #puts delivery
-    end
-  end
     end
   end
 end
